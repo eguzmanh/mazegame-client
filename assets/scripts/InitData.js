92e5725722d2cea943aecc70011695097ee88b7f
Eduardo Guzman <eguzman6@csus.edu>
2023-05-13 23:30:42 -0700

    adding my script changes

--- assets/scripts/InitData.js
+++ assets/scripts/InitData.js
@@ -4,4 +4,8 @@ var numFoodStations = 30;
 // You can choose the following values: Green, Purple, Red, or Yellow
 var playerColor = "Red"
 var lives = 3
+var axisLineLength = 1000.0
+var walkSoundMaxDistance = 1.0
+var walkSoundVolume = 100
+var backgroundMusicVolume = 15
 // var playerLocation = [0,0,0]
